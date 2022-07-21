4859d1420c85dcf59d74c59735d5eec4d5879b7e
Gerardo Hernandez <hernandez.gerardo@bcgplatinion.com>
2022-07-21 16:05:54 -0400

    primer commit

---
+++ useTodo.js
@@ -0,0 +1,62 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../09-useReducer/todoReducer";
+
+
+const init = () => {
+    return JSON.parse( localStorage.getItem( 'todos' ) ) || [];
+}
+
+// const initialState = [
+//     {
+//     	id: new Date().getTime(),
+//     	description: 'Recolectar la piedra del alma',
+//     	done: false,
+//     },
+// ];
+
+/**
+ * useTodo custom hook
+ * @returns 
+ */
+export const useTodo = () => {
+    const [ todos, dispatchTodoAction ] = useReducer( todoReducer, [], init );
+
+    useEffect( () => {
+        localStorage.setItem( 'todos', JSON.stringify( todos ) || [] );
+    }, [ todos ] )
+
+
+    const handleNewTodo = ( todo ) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo,
+        };
+
+        dispatchTodoAction( action );
+    };
+
+    const handleDeleteTodo = ( id ) => {
+        const action = {
+            type: '[TODO] Remove Todo',
+            payload: id
+        };
+        dispatchTodoAction( action );
+    }
+
+    const handleToggleTodo = ( id ) => {
+        const action = {
+            type: '[TODO] Toggle Todo',
+            payload: id
+        };
+        dispatchTodoAction( action );
+    }
+
+    return {
+        todos,
+        handleDeleteTodo,
+        handleToggleTodo,
+        handleNewTodo,
+        todosCount: todos.length,
+        pendientTodosCount: todos.filter( todo => !todo.done ).length,
+    }
+};
